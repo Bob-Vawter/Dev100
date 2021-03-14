ac200b3e4d571925065ccd57710a194f689000a4
Bob Vawter <bobvawter@msn.com>
2021-03-14 17:35:15 -0400

    super review class

--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -10,13 +10,21 @@ console.log(num)
 
 //--- Medium
 //create a variable that holds a value from the input
-
+var num2 = Number(document.querySelector('input').value)
 
 //add 25 to that number
+num2 += 25
 
 //alert that number
+alert(num2)
 
 //--- Hard
 //create a variable that holds the h1
+var h1Hold = document.querySelector('h1')
 
 //add an event listener to that element that console logs the sum of the two previous variables
+h1Hold.addEventListener('click',fillH1)
+
+function fillH1(){
+  h1Hold.innerHTML = num + num2
+}
